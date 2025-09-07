168dba036c4721694b7bc67c0ba11b5d1ea0b6fd
luAr26101 <raul.cosmin.26@icloud.com>
2025-09-07 19:35:38 +0300

    Add pagination to the blog page.

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Pagination from "~/components/Pagination";
 import PostCard from "~/components/PostCard";
 import type { PostMeta } from "~/types";
 import type { Route } from "./+types/index";
@@ -20,13 +22,29 @@ export async function loader({
 }
 
 function BlogPage({ loaderData }: Route.ComponentProps) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
   const { posts } = loaderData;
+
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+
+  const currentPosts = posts.slice(indexOfFirst, indexOfLast);
   return (
     <div className="mx-auto mt-10 max-w-3xl bg-gray-900 px-6 py-6">
       <h2 className="mb-8 text-3xl font-bold text-white">📝 Blog</h2>
-      {posts.map((post) => (
+      {currentPosts.map((post) => (
         <PostCard key={post.slug} post={post} />
       ))}
+
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      )}
     </div>
   );
 }
