879520f9d5709b0caa36e055fded862dee937c43
luAr26101 <raul.cosmin.26@icloud.com>
2025-09-07 12:29:59 +0300

    Display featured projects on the home page.

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,3 +1,5 @@
+import FeaturedProjects from "~/components/FeaturedProjects";
+import type { Project } from "~/types";
 import type { Route } from "./+types/index";
 
 export function meta({}: Route.MetaArgs) {
@@ -7,6 +9,24 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <>HomePage</>;
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
+  const data = await res.json();
+
+  return {
+    projects: data,
+  };
 }
+
+function HomePage({ loaderData }: Route.ComponentProps) {
+  const { projects } = loaderData;
+  return (
+    <>
+      <FeaturedProjects projects={projects} count={1} />
+    </>
+  );
+}
+
+export default HomePage;
