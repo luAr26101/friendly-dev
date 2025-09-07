3cd9c0d27642cf4cba7f10af6ea3246b31acde55
luAr26101 <raul.cosmin.26@icloud.com>
2025-09-07 14:34:16 +0300

    Fetch post metadata.

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,22 @@
-function BlogPage() {
+import type { PostMeta } from "~/types";
+import type { Route } from "./+types/index";
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL("/posts-meta.json", request.url);
+  const res = await fetch(`${url.href}`);
+
+  if (!res.ok) throw new Error("Failed to fetch data.");
+
+  const data = await res.json();
+
+  return { posts: data };
+}
+
+function BlogPage({ loaderData }: Route.ComponentProps) {
+  const { posts } = loaderData;
+  console.log(posts);
   return (
     <>
       <h2 className="mb-8 text-3xl font-bold text-white">📝 Blog</h2>
