dbe3df198d9fb0521877c052c06d9a06579cb787
luAr26101 <raul.cosmin.26@icloud.com>
2025-09-07 10:06:54 +0300

    Add the for the projects.

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,17 @@
-function ProjectsPage() {
+import type { Route } from "./+types/index";
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const res = await fetch("http://localhost:8000/projects");
+  const data = await res.json();
+
+  return {
+    projects: data,
+  };
+}
+
+function ProjectsPage({ loaderData }: Route.ComponentProps) {
+  const { projects } = loaderData;
+  console.log(projects);
   return (
     <>
       <h2 className="mb-8 text-3xl font-bold text-white">🚀 Projects</h2>
