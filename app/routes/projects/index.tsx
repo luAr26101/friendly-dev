11d5ce9faee7162b6b90c4d00cd23ed17eaccbee
luAr26101 <raul.cosmin.26@icloud.com>
2025-09-07 11:41:04 +0300

    Project category filter.

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -16,25 +16,50 @@ export async function loader({
 }
 
 function ProjectsPage({ loaderData }: Route.ComponentProps) {
-  const { projects } = loaderData as { projects: Project[] };
+  const [selectedCategory, setSelectedCategory] = useState("All");
   const [currentPage, setCurrentPage] = useState(1);
 
   const projectsPerPage = 10;
+  const { projects } = loaderData as { projects: Project[] };
+
+  // Get unique categories
+  const categories = [
+    "All",
+    ...new Set(projects.map((project) => project.category)),
+  ];
+
+  // Filter projects base on category
+  const filteredProjects =
+    selectedCategory === "All"
+      ? projects
+      : projects.filter((project) => project.category === selectedCategory);
 
   // Calculate totalPages
-  const totalPages = Math.ceil(projects.length / projectsPerPage);
+  const totalPages = Math.ceil(filteredProjects.length / projectsPerPage);
 
   // Get curent page's projects
   const indexOfLast = currentPage * projectsPerPage;
   const indexOfFirst = indexOfLast - projectsPerPage;
 
-  const currentProjects = projects.slice(indexOfFirst, indexOfLast);
-
-  // Pagination button render
+  const currentProjects = filteredProjects.slice(indexOfFirst, indexOfLast);
 
   return (
     <>
       <h2 className="mb-8 text-3xl font-bold text-white">🚀 Projects</h2>
+      <div className="mb-8 flex flex-wrap gap-2">
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => {
+              setSelectedCategory(category);
+              setCurrentPage(1);
+            }}
+            className={`cursor-pointer rounded px-3 py-1 text-sm ${selectedCategory === category ? "bg-blue-600 text-white" : "bg-gray-700 text-gray-200"}`}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
       <div className="sm: grid grid-cols-2 gap-6">
         {currentProjects.map((project) => (
           <ProjectCard project={project} key={project.id} />
