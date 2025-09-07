0078b5617e6c6e2cc642ec911bcd919c176ed8c5
luAr26101 <raul.cosmin.26@icloud.com>
2025-09-07 09:16:07 +0300

    Add navigation menu and hero section.

---
+++ app/components/Hero.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router";
+
+function Hero({
+  name = "[NAME]",
+  text = "I build friendly web experiences and help others become confident modern developers.",
+}) {
+  return (
+    <header className="bg-gray-900 px-4 py-20 text-center text-white transition-colors duration-300">
+      <h2 className="mb-4 text-4xl font-bold">Hey, I'm {name} 🙋‍♂️</h2>
+      <p className="mx-auto mb-6 max-w-2xl text-lg text-gray-300">{text}</p>
+      <div className="flex justify-center gap-4">
+        <Link
+          to="/projects"
+          className="rounded bg-blue-600 px-6 py-2 text-white transition hover:bg-blue-700"
+        >
+          View Projects
+        </Link>
+        <Link
+          to="/contact"
+          className="rounded border border-blue-500 px-6 py-2 text-blue-400 transition hover:bg-blue-600 hover:text-white"
+        >
+          Contact Me
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Hero;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+
+import { FaBars, FaLaptopCode, FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router";
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const base = "transition hover:text-blue-400";
+  const active = "text-blue-400 font-semibold";
+
+  const closeMenu = () => setMenuOpen(false);
+  return (
+    <nav className="md sticky top-0 z-50 border-b border-gray-700 bg-gray-800 shadow">
+      <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
+        <NavLink
+          to="/"
+          className="flex items-center gap-2 text-lg font-bold text-blue-300 hover:text-red-500"
+        >
+          <FaLaptopCode className="text-xl text-blue-400" />
+          <span>The Friendly Developer</span>
+        </NavLink>
+        {/* Desktop nav */}
+        <div className="hidden items-center gap-6 md:flex">
+          <div className="space-x-4 text-sm text-gray-300">
+            <NavLink
+              className={({ isActive }) => (isActive ? active : base)}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? active : base)}
+              to="/projects"
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? active : base)}
+              to="/blog"
+            >
+              Blog
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? active : base)}
+              to="/about"
+            >
+              About
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? active : base)}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </div>
+        </div>
+
+        {/* Mobile nav button */}
+        <div className="flex items-center gap-4 md:hidden">
+          <button
+            onClick={() => setMenuOpen((prevMenuOpen) => !prevMenuOpen)}
+            className="cursor-pointer text-xl text-blue-400"
+            title="Menu"
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
+      </div>
+      {/* Mobile navigation list */}
+      {menuOpen && (
+        <div className="space-y-2 space-x-4 border-t border-gray-700 bg-gray-800 px-6 py-4 text-center md:hidden">
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to="/"
+            onClick={closeMenu}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to="/projects"
+            onClick={closeMenu}
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to="/blog"
+            onClick={closeMenu}
+          >
+            Blog
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to="/about"
+            onClick={closeMenu}
+          >
+            About
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to="/contact"
+            onClick={closeMenu}
+          >
+            Contact
+          </NavLink>
+        </div>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
